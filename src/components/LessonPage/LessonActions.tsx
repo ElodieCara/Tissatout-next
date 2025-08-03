bdfeefc31ddf6de841128359cc2b7aeb9de0f1fb
Elodie Caradeuc <caradeuc.elodie@gmail.com>
2025-08-03 12:10:23 +0200

    ✨ feat: uniformise le typage ContentItem, corrige le mapping données, ajoute le bouton ‘Fiche à imprimer’ aux leçons

--- src/components/LessonPage/LessonActions.tsx
+++ src/components/LessonPage/LessonActions.tsx
@@ -3,17 +3,25 @@
 import Button from "@/components/Button/Button";
 
 export default function LessonActions({ slug }: { slug: string }) {
+
+    const handlePrint = () => {
+        const body = document.body;
+
+        window.print();
+    };
+
     return (
         <div className="lesson__actions">
-            <Button href={`/fiches/${slug}.pdf`} variant="yellow-button">
+            <Button onClick={handlePrint} variant="yellow-button">
                 Fiche à imprimer
             </Button>
             {/* <Button href={`/quiz/${slug}`} variant="yellow-button">
                 Faire le quiz
             </Button> */}
-            <Button href={`/illustrations/${slug}`} variant="yellow-button">
+            {/* <Button href={`/illustrations/${slug}`} variant="yellow-button">
                 Voir l’image
-            </Button>
+            </Button> */}
         </div>
     );
 }
+
